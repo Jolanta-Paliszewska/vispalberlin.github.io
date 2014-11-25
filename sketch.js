bda400fc2c38ab1b9e73303685e002ba8fa922ea
vispalberlin <j.vispal@me.com>
2014-11-26 00:31:31 +0100

    initial commit

--- sketch.js
+++ sketch.js
@@ -23,16 +23,21 @@ function move (e) {
 }*/
 
 var imgObj = null;
+var animate;
 function init(){
    imgObj = document.getElementById('displayed');
-   imgObj.style.position= 'relative'; 
-   imgObj.style.left = '0px'; 
+   imgObj.style.position= 'relative';
+   imgObj.style.left = '0px';
 }
 
 function moveRight(){
    imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+   animate = setTimeout(moveRight,10); // call moveRight in 20msec
+}
+
+function stop(){
+   clearTimeout(animate);
+   imgObj.style.left = '0px';
 }
 
 window.onload =init;
-
-
