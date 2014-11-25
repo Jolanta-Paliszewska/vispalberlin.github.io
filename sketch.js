306056a192eb3d5ca3cf8ba77d1f5af9d519cefb
vispalberlin <j.vispal@me.com>
2014-11-26 00:34:52 +0100

    initial commit

--- sketch.js
+++ sketch.js
@@ -9,18 +9,7 @@ function draw() {
   //background(255);
   //ellipse(500,100,100,100);
 }
-/*var x;
-var y;
-var bool = 1;
 
-function move (e) {
-	x = e.clientX;
-	y = e.clientY;
-	//var displayed = document.getElementById("displayed");
-	document.getElementById("displayed").style.top = y + "px";
-	document.getElementById("displayed").style.left = x + "px";
-
-}*/
 
 var imgObj = null;
 var animate;
